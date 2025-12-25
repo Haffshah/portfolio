9e8ea42d44124698f497b63ac2343fc9267ed305
Harsh Shah <71213359+Haffshah@users.noreply.github.com>
2025-12-25 19:33:02 +0530

    New Theme Changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "729015497"
+    serviceWorkerVersion: "1534296139"
   }
 });
