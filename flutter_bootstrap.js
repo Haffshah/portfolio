7ebb646dc8391a1c538a838c5cae4218b368d6d5
Harsh Shah <71213359+Haffshah@users.noreply.github.com>
2025-12-31 22:06:03 +0530

    Routes maanged

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1616235471"
+    serviceWorkerVersion: "816568467"
   }
 });
